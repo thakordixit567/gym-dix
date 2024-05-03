654c0d194297dc5ff7dbc69a535b2b7ff3d853d6
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-05-03 12:24:25 +0530

    UPDATE!

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const HeroBanner = () => {
+  return (
+    <Box>
+    <Typography>
+     Fitness Club
+    </Typography>
+    </Box>
+  )
+};
+
+export default HeroBanner;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,8 @@ const Navbar = () => {
     <Stack
      direction='row'
      justifyContent='space-around'
-     sx= {{ gap:{sm :'122px',xs: '40px'}, mt:{ sm: '32px',xs: '20px'}}}
+     sx= {{ gap:{sm :'122px',xs: '40px'}, mt:{ sm: '32px',xs: '20px'},justifyContent:'none'}}
+     px="20px"
     >
       <Link to="/">
         <img
