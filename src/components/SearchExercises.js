c61f35a3747fedffe8d4d3ff6fcd5ad4dadf9d12
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-05-05 14:34:11 +0530

    UPDATE!

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -38,13 +38,13 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+      <Typography fontWeight={700} color="#ffffff" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
         Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
           height="76px"
-          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '24px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,22 @@
 export const exerciseOptions = {
-  method: "GET",
-  params: { limit: "10" },
+  method: 'GET',
   headers: {
-   
-    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY
-  }
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '5362135a5amsh3e1f8a6dafb2296p143837jsna92365529849',
+  },
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
   return data;
 };
